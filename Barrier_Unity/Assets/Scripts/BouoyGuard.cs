2979a271d8221bca8e24090a4d732a39158ed907
Nikon <parvitskiyn@gmail.com>
2021-10-23 19:31:16 +0300

    spline zone. some files deleted

--- Barrier_Unity/Assets/Scripts/BouoyGuard.cs
+++ Barrier_Unity/Assets/Scripts/BouoyGuard.cs
@@ -5,153 +5,151 @@ using UnityEngine;
 
 public class BouoyGuard : MonoBehaviour
 {
-   public Transform m_torpedo;
-   public Transform m_bouy1;
-   public Transform m_bouy2;
-   public float m_range = 5000f;
-   public float m_bearingRange;
-   public float m_bearingError;
+    public Transform m_torpedo;
+    public GameObject m_bouy1;
+    public GameObject m_bouy2;
+    public float m_range = 5000f;
+    public float m_bearingRange;
+    public float m_bearingError;
+    public Material m_material;
 
-   public Material m_material;
-   
-   void Start()
-   {
-      m_mesh = new GameObject("zone");
+    private List<GameObject> ellipse_points;
+    private float m_errorBeg;
+    private float m_errorEnd;
+    private float m_errorCur;
+    private float m_errorTime;
 
-      m_meshFilter = m_mesh.AddComponent<MeshFilter>();
-      m_meshFilter.mesh = Utils.CreateRombusMesh(Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero);
-      m_meshRenderer = m_mesh.AddComponent<MeshRenderer>();
-      m_meshRenderer.material = m_material;
-      //m_mesh.transform.localPosition = new Vector3(0, 0.25f, 0);
+    private GameObject m_rombZone;
+    private GameObject m_elipseZone;
+    private GameObject m_splineZone;
+    void Start()
+    {
+        m_rombZone = new GameObject("romb_zone");
+        m_rombZone.AddComponent<MeshFilter>().mesh = Utils.CreateRombusMesh(Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero);
+        m_rombZone.AddComponent<MeshRenderer>().material = m_material;
 
+        m_elipseZone = new GameObject("elipse_zone");
+        m_elipseZone.AddComponent<MeshFilter>().mesh = Utils.CreateEllipseMesh(Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero);
+        m_elipseZone.AddComponent<MeshRenderer>().material = m_material;
 
-      m_errorBeg = Random.Range(-m_bearingError/2f, m_bearingError/2);
-      m_errorCur = m_errorBeg;
-      m_errorEnd = Random.Range(-m_bearingError / 2f, m_bearingError / 2);
-      m_errorTime = Time.time;
+        m_splineZone = new GameObject("spline_zone");
+        m_splineZone.AddComponent<MeshFilter>().mesh = Utils.CreateSplineMesh(Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero);
+        m_splineZone.AddComponent<MeshRenderer>().material = m_material;
 
-      StartCoroutine(timerCoroutine());
-   }
-
-   IEnumerator timerCoroutine()
-   {
-      while(true)
-      {
-         yield return new WaitForSeconds(1);
-
-         m_errorBeg = m_errorEnd;
-         m_errorEnd = Random.Range(-m_bearingError / 2f, m_bearingError / 2f);
-         m_errorTime = Time.time;
-      }
-   }
-
-   // Update is called once per frame
-   void Update()
-   {
-      m_errorCur = Mathf.Lerp(m_errorBeg, m_errorEnd, Time.time - m_errorTime);
-
-      Vector3 vc = Quaternion.AngleAxis(m_errorCur, Vector3.up) * (m_torpedo.position - m_bouy1.position).normalized;
-      Vector3 vr = getDir(vc, m_bearingRange / 2);
-      Vector3 vl = getDir(vc, -m_bearingRange / 2);
-
-      Vector3 p1 = m_bouy1.position;
-      Vector3 p1r = m_bouy1.position + vr * m_range + Vector3.up;
-      Vector3 p1l = m_bouy1.position + vl * m_range + Vector3.up;
-
-      vc = Quaternion.AngleAxis(m_errorCur, Vector3.up) * (m_torpedo.position - m_bouy2.position).normalized;
-      vr = getDir(vc, m_bearingRange / 2);
-      vl = getDir(vc, -m_bearingRange / 2);
-
-      Vector3 p2 = m_bouy2.position;
-      Vector3 p2r = m_bouy2.position + vr * m_range + Vector3.up;
-      Vector3 p2l = m_bouy2.position + vl * m_range + Vector3.up;
-
-      Vector3 c1 = getCross(p1, p1l, p2, p2l);
-      Vector3 c2 = getCross(p1, p1r, p2, p2l);
-      Vector3 c3 = getCross(p1, p1r, p2, p2r);
-      Vector3 c4 = getCross(p1, p1l, p2, p2r);
-      
-      Vector3[] vertices = { c1-c1, c2-c1, c3-c1, c4-c1};
-      
-      m_meshFilter.mesh.vertices = vertices;
-
-      m_mesh.transform.position = new Vector3(c1.x, 1, c1.z);
-
-      m_meshRenderer.material.color =new Color(1, 0, 0, Mathf.PingPong(Time.time, 0.5f));
-   }
-
-   private void OnDrawGizmos()
-   {
- 
-   //   Gizmos.color = Color.magenta;
-
-   //   Vector3 vc = Quaternion.AngleAxis(m_errorCur, Vector3.up) * (m_torpedo.position - m_bouy1.position).normalized;
-   //   Vector3 vr = getDir(vc, m_bearingRange/2);
-   //   Vector3 vl = getDir(vc, -m_bearingRange/2);
-
-   //   Vector3 p1 = m_bouy1.position;
-   //   Vector3 p1r = m_bouy1.position + vr * m_range + Vector3.up;
-   //   Vector3 p1l = m_bouy1.position + vl * m_range + Vector3.up;
-      
-   //   Gizmos.DrawLine(p1, p1r);
-   //   Gizmos.DrawLine(p1, p1l);
+        //m_rombZone.transform.localPosition = new Vector3(0, 0.25f, 0);
 
 
-   //   vc = Quaternion.AngleAxis(m_errorCur, Vector3.up) * (m_torpedo.position - m_bouy2.position).normalized;
-   //   vr = getDir(vc, m_bearingRange / 2);
-   //   vl = getDir(vc, -m_bearingRange / 2);
+        m_errorBeg = Random.Range(-m_bearingError / 2f, m_bearingError / 2);
+        m_errorCur = m_errorBeg;
+        m_errorEnd = Random.Range(-m_bearingError / 2f, m_bearingError / 2);
+        m_errorTime = Time.time;
 
-   //   Vector3 p2 = m_bouy2.position;
-   //   Vector3 p2r = m_bouy2.position + vr * m_range + Vector3.up;
-   //   Vector3 p2l = m_bouy2.position + vl * m_range + Vector3.up;
-      
-   //   Gizmos.DrawLine(p2, p2r);
-   //   Gizmos.DrawLine(p2, p2l);
+        ellipse_points = new List<GameObject>();
+        for (int i = 0; i < 6; i++)
+        {
+            GameObject new_sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            new_sphere.transform.localScale = new Vector3(5, 5, 5);
+            new_sphere.name = $"{i} point";
+            ellipse_points.Add(new_sphere);
+        }
 
-   //   Gizmos.color = Color.red;
+        StartCoroutine(timerCoroutine());
+    }
 
-   //   Vector3 c1 = getCross(p1, p1l, p2, p2l);
-   //   Vector3 c2 = getCross(p1, p1r, p2, p2l);
-   //   Vector3 c3 = getCross(p1, p1r, p2, p2r);
-   //   Vector3 c4 = getCross(p1, p1l, p2, p2r);
+    IEnumerator timerCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
 
+            m_errorBeg = m_errorEnd;
+            m_errorEnd = Random.Range(-m_bearingError / 10f, m_bearingError / 10f);
+            m_errorTime = Time.time;
+        }
+    }
 
-   //   Gizmos.DrawCube(c1, new Vector3(50, 10, 50));
-   //   Gizmos.DrawCube(c2, new Vector3(50, 10, 50));
-   //   Gizmos.DrawCube(c3, new Vector3(50, 10, 50));
-   //   Gizmos.DrawCube(c4, new Vector3(50, 10, 50));
-   }
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_bouy1 == null || m_bouy2 == null)
+            return;
 
-   private Vector3 getCross(Vector3 p11, Vector3 p12, Vector3 p21, Vector3 p22)
-   {
-      p11.y = 1;
-      p12.y = 1;
-      p21.y = 1;
-      p22.y = 1;
+        m_errorCur = Mathf.Lerp(m_errorBeg, m_errorEnd, Time.time - m_errorTime);
 
-      Vector3 f1 = Vector3.Cross(p11, p12);
-      Vector3 f2 = Vector3.Cross(p21, p22);
+        Vector3 vc = Quaternion.AngleAxis(m_errorCur, Vector3.up) * (m_torpedo.position - m_bouy1.transform.position).normalized;
+        Vector3 vr = getDir(vc, m_bearingRange / 2);
+        Vector3 vl = getDir(vc, -m_bearingRange / 2);
 
-      Vector3 cross = Vector3.Cross(f1, f2);
+        Vector3 p1 = m_bouy1.transform.position;
+        Vector3 p1r = m_bouy1.transform.position + vr * m_range + Vector3.up;
+        Vector3 p1l = m_bouy1.transform.position + vl * m_range + Vector3.up;
 
-      cross.x = cross.x / cross.y;
-      cross.z = cross.z / cross.y;
-      cross.y = 0;
+        vc = Quaternion.AngleAxis(m_errorCur, Vector3.up) * (m_torpedo.position - m_bouy2.transform.position).normalized;
+        vr = getDir(vc, m_bearingRange / 2);
+        vl = getDir(vc, -m_bearingRange / 2);
 
-      return cross;
-   }
+        Vector3 p2 = m_bouy2.transform.position;
+        Vector3 p2r = m_bouy2.transform.position + vr * m_range + Vector3.up;
+        Vector3 p2l = m_bouy2.transform.position + vl * m_range + Vector3.up;
 
-   private Vector3 getDir(Vector3 v, float a)
-   { 
-      return Quaternion.AngleAxis(a, Vector3.up) * v;
-   }
+        Vector3 c1 = getCross(p1, p1l, p2, p2l);
+        Vector3 c2 = getCross(p1, p1r, p2, p2l);
+        Vector3 c3 = getCross(p1, p1r, p2, p2r);
+        Vector3 c4 = getCross(p1, p1l, p2, p2r);
 
-   private float m_errorBeg;
-   private float m_errorEnd;
-   private float m_errorCur;
-   private float m_errorTime;
+        Vector3[] vertices = { c1 - c1, c2 - c1, c3 - c1, c4 - c1 };
+        ellipse_points[0].transform.position = c1;
+        ellipse_points[1].transform.position = c2;
+        ellipse_points[2].transform.position = c3;
+        ellipse_points[3].transform.position = c4;
+        ellipse_points[4].transform.position = new Vector3((c1.x + c3.x) / 2f, 10, (c1.z + c3.z) / 2f);
+        ellipse_points[5].transform.position = new Vector3((c2.x + c4.x) / 2f, 10, (c2.z + c4.z) / 2f);
 
-   GameObject m_mesh;
-   MeshFilter m_meshFilter;
-   MeshRenderer m_meshRenderer;
+        m_rombZone.GetComponent<MeshFilter>().mesh = Utils.CreateRombusMesh(vertices[0], vertices[1], vertices[2], vertices[3]);
+        m_rombZone.transform.position = new Vector3(c1.x, 10, c1.z);
+        m_rombZone.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, Mathf.PingPong(Time.time, 0.5f));
+
+        float magn1 = (vertices[0] - vertices[2]).magnitude / 2;
+        float magn2 = (vertices[1] - vertices[3]).magnitude / 2;
+        if (magn2 > magn1)
+        {
+            m_elipseZone.GetComponent<MeshFilter>().mesh = Utils.CreateEllipseMesh(vertices[0], vertices[1], vertices[2], vertices[3]);
+            m_elipseZone.transform.eulerAngles = new Vector3(0, 180 - Vector3.SignedAngle((c2 - c4).normalized, Vector3.forward, Vector3.up), 0);
+        }
+        else
+        {
+            m_elipseZone.GetComponent<MeshFilter>().mesh = Utils.CreateEllipseMesh(vertices[1], vertices[2], vertices[3], vertices[0]);
+            m_elipseZone.transform.eulerAngles = new Vector3(0, 180 - Vector3.SignedAngle((c1 - c3).normalized, Vector3.forward, Vector3.up), 0);
+        }
+        m_elipseZone.transform.position = new Vector3((c2.x + c4.x) / 2f, 10, (c2.z + c4.z) / 2f);
+        m_elipseZone.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0, Mathf.PingPong(Time.time, 0.5f));
+
+        m_splineZone.GetComponent<MeshFilter>().mesh = Utils.CreateSplineMesh(vertices[0], vertices[1], vertices[2], vertices[3]);
+        m_splineZone.transform.position = new Vector3(c1.x, 10, c1.z);
+        m_splineZone.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 1, Mathf.PingPong(Time.time, 0.5f));
+    }
+
+    private Vector3 getCross(Vector3 p11, Vector3 p12, Vector3 p21, Vector3 p22)
+    {
+        p11.y = 1;
+        p12.y = 1;
+        p21.y = 1;
+        p22.y = 1;
+
+        Vector3 f1 = Vector3.Cross(p11, p12);
+        Vector3 f2 = Vector3.Cross(p21, p22);
+
+        Vector3 cross = Vector3.Cross(f1, f2);
+
+        cross.x = cross.x / cross.y;
+        cross.z = cross.z / cross.y;
+        cross.y = 0;
+
+        return cross;
+    }
+
+    private Vector3 getDir(Vector3 v, float a)
+    {
+        return Quaternion.AngleAxis(a, Vector3.up) * v;
+    }
 }
--- Barrier_Unity/Assets/Scripts/SectorDrawer.cs
+++ Barrier_Unity/Assets/Scripts/SectorDrawer.cs
@@ -13,61 +13,86 @@ struct sector
 
 public class SectorDrawer : MonoBehaviour
 {
-   [SerializeField] GameObject m_origin;
-   [SerializeField] int m_sectorSubdivNum = 100;
-   [SerializeField] Material m_sectorMaterial;
-   sector[] m_sectors;
-   GameObject[] m_sectorMeshes;
-   // Start is called before the first frame update
-   void Awake()
-   {
-      sector s1;
-      s1.range_min = 100;
-      s1.range_max = 200;
-      s1.width = 80;
-      s1.color = new Color(0.9f, 0.1f, 0.1f, 0.5f);
+    [SerializeField] GameObject m_origin;
+    [SerializeField] int m_sectorSubdivNum = 100;
+    [SerializeField] Material m_sectorMaterial;
+    sector[] m_sectors;
+    GameObject[] m_sectorMeshes;
+    [SerializeField] public float a = 3;
+    [SerializeField] public float b = 5;
+    [SerializeField] public float h = 10;
+    [SerializeField] public float k = 10;
+    [SerializeField] public float theta = 45;
+    [SerializeField] public int resolution = 1000;
+    Vector3[] positions;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sector s1;
+        s1.range_min = 100;
+        s1.range_max = 200;
+        s1.width = 80;
+        s1.color = new Color(0.9f, 0.1f, 0.1f, 0.5f);
 
-      sector s2;
-      s2.range_min = 1000;
-      s2.range_max = 2000;
-      s2.width = 80;
-      s2.color = new Color(0.5f, 0.1f, 0.8f, 0.5f);
+        sector s2;
+        s2.range_min = 1000;
+        s2.range_max = 2000;
+        s2.width = 80;
+        s2.color = new Color(0.5f, 0.1f, 0.8f, 0.5f);
 
 
-      m_sectors = new sector[] { s1, s2 };
+        m_sectors = new sector[] { s1, s2 };
 
-      var meshesList = new List<GameObject>();
-      foreach (var sec in m_sectors)
-         meshesList.Add(createSectorMesh(sec));
+        var meshesList = new List<GameObject>();
+        foreach (var sec in m_sectors)
+            meshesList.Add(createSectorMesh(sec));
 
-      m_sectorMeshes = meshesList.ToArray();
+        m_sectorMeshes = meshesList.ToArray();
 
-   }
-   void Start()
-   {
-   }
+    }
+    void Start()
+    {
+    }
 
-   // Update is called once per frame
-   void Update()
-   {
+    // Update is called once per frame
+    void Update()
+    {
 
-   }
-   public void ShowSectors(bool draw)
-   {
-      foreach (var m in m_sectorMeshes)
-         m.SetActive(draw);
-   }
+    }
 
-   GameObject createSectorMesh(sector s)
-   {
-      var g = new GameObject("Sector");
-      g.AddComponent<MeshFilter>().mesh = Utils.CreateSectorMesh(s.width, s.range_min, s.range_max, m_sectorSubdivNum);
-      var meshReneder = g.AddComponent<MeshRenderer>();
-      meshReneder.material = m_sectorMaterial;
-      meshReneder.material.color = s.color;
 
-      g.transform.SetParent(m_origin.transform, false);
-      return g;
-   }
+    public void ShowSectors(bool draw)
+    {
+        foreach (var m in m_sectorMeshes)
+            m.SetActive(draw);
+    }
 
+    GameObject createSectorMesh(sector s)
+    {
+        var g = new GameObject("Sector");
+        g.AddComponent<MeshFilter>().mesh = Utils.CreateSectorMesh(s.width, s.range_min, s.range_max, m_sectorSubdivNum);
+        var meshReneder = g.AddComponent<MeshRenderer>();
+        meshReneder.material = m_sectorMaterial;
+        meshReneder.material.color = s.color;
+
+        g.transform.SetParent(m_origin.transform, false);
+        return g;
+    }
+
+    Vector3[] CreateEllipse(float a, float b, float h, float k, float theta, int resolution)
+    {
+
+        positions = new Vector3[resolution + 1];
+        Quaternion q = Quaternion.AngleAxis(theta, Vector3.forward);
+        Vector3 center = new Vector3(h, k, 0.0f);
+
+        for (int i = 0; i <= resolution; i++)
+        {
+            float angle = (float)i / (float)resolution * 2.0f * Mathf.PI;
+            positions[i] = new Vector3(a * Mathf.Cos(angle), b * Mathf.Sin(angle), 0.0f);
+            positions[i] = q * positions[i] + center;
+        }
+
+        return positions;
+    }
 }
