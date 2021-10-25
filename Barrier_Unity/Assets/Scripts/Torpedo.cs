2d8a8459654534cb36d91154774debde2df63c8e
Nikon <parvitskiyn@gmail.com>
2021-10-26 01:19:03 +0300

    рисуется путь торпеды

--- Barrier_Unity/Assets/Scripts/Torpedo.cs
+++ Barrier_Unity/Assets/Scripts/Torpedo.cs
@@ -13,15 +13,25 @@ public class Torpedo : MonoBehaviour
    [SerializeField] Material m_circleMaterial;
    [SerializeField] GameObject m_arrow;
    [SerializeField] GameObject m_targetShip;
+   
+   [SerializeField] int _distBeetweenWaysPoints = 30;
 
    GameObject m_circleObject;
    GameObject m_arrowedLineObject;
+   GameObject m_torpedoWay;
+
+   Vector3 _lastPos;
+   List<Vector3> _wayPoints;
 
    // Start is called before the first frame update
    void Start()
    {
       //m_circleObject = createCircle();
       m_arrowedLineObject = createForwardArrowedLine();
+      _lastPos = gameObject.transform.position;
+      _wayPoints = new List<Vector3>();
+      _wayPoints.Add(_lastPos);
+      m_torpedoWay = createWay();
    }
 
    // Update is called once per frame
@@ -30,6 +40,7 @@ public class Torpedo : MonoBehaviour
 
       if (!Scenario.Instance.IsRunning)
          return;
+
 
       float step = m_speed * Time.deltaTime;
       Vector3 pos = transform.position;
@@ -48,6 +59,13 @@ public class Torpedo : MonoBehaviour
       //   new Color(1, 0, 0, Mathf.PingPong(Time.time, 0.5f));
       //float scaleFactor = Mathf.PingPong(Time.time * 0.5f, 0.7f) + 0.3f;
       //m_circleObject.transform.localScale = new Vector3(scaleFactor, 1, scaleFactor);
+
+      if ((_wayPoints.Last() - gameObject.transform.position).magnitude > _distBeetweenWaysPoints)
+      {
+         _lastPos = gameObject.transform.position;
+         _wayPoints.Add(_lastPos);
+         m_torpedoWay.GetComponent<MeshFilter>().mesh = Utils.CreateOfssetedLinedMesh(_wayPoints, _distBeetweenWaysPoints);
+      }
    }
 
    GameObject createCircle()
@@ -67,13 +85,23 @@ public class Torpedo : MonoBehaviour
    {
       var g = new GameObject("ArrowLine");
       g.layer = 6;
-      Mesh mesh = new Mesh();
       g.AddComponent<MeshFilter>().mesh = Utils.CreateLinedMesh(m_arrowLength, m_arrowWidth, m_arrowWidth * 2);
       var meshReneder = g.AddComponent<MeshRenderer>();
       meshReneder.material = m_arrow.GetComponent<MeshRenderer>().material;
 
       g.transform.SetParent(transform, false);
       g.transform.localPosition = new Vector3(0, 0.5f, 0);
+      return g;
+   }
+
+   private GameObject createWay()
+   {
+      var g = new GameObject("TorpedoTail");
+      //g.layer = 6;
+      g.AddComponent<MeshFilter>().mesh = Utils.CreateOfssetedLinedMesh(_wayPoints, 50);
+      var meshReneder = g.AddComponent<MeshRenderer>();
+      meshReneder.material = m_arrow.GetComponent<MeshRenderer>().material;
+
       return g;
    }
 
