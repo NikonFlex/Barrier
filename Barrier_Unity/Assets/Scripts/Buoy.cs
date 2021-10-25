779c85d370c3ff55f2f5c705999de35c4fae7703
Nikon <parvitskiyn@gmail.com>
2021-10-25 16:08:25 +0300

    files renamed

---
+++ Barrier_Unity/Assets/Scripts/Buoy.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BuoyState
+{
+   None,
+   PreparingToWork,
+   Working
+}
+
+public class Buoy : MonoBehaviour
+{
+   public BuoyState State => _state;
+   private BuoyState _state = BuoyState.None;
+   void Start()
+   {
+      Scenario.Instance.OnBouyHatched(this);
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (_state == BuoyState.None)
+         StartCoroutine(preparingToWork());
+   }
+
+   IEnumerator preparingToWork()
+   {
+      _state = BuoyState.PreparingToWork;
+      yield return new WaitForSeconds(VarSync.GetFloat(VarName.BuoyReadyTime));
+      _state = BuoyState.Working;
+      print("bouy start work");
+      FindObjectOfType<BuoyGuard>().GetComponent<BuoyGuard>().AddBuoy(gameObject);
+      yield return null;
+   }
+}
