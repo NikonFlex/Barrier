b892d241d0f4ff3344b40745ffee52539c23e9e7
lokolokoa <tuvaev@gmail.com>
2022-01-23 22:37:52 +0300

    эффект зоны поражения ракет

---
+++ Barrier_Unity/Assets/LineRenderScale.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class LineRenderScale : MonoBehaviour
+{
+
+    Renderer rend;
+    LineRenderer line;
+    public float _kx = 1;
+    public float _ky = 1;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+       
+    }
+
+    void Update()
+    {
+        line.material.SetTextureScale("_MainTex", new Vector2(_kx, _ky));
+    }
+}
